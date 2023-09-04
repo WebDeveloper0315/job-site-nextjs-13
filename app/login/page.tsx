24268df17e44ddf73aa90aa5913d6ea3b1894104
Blackghost0315 <blackghost1503@gmail.com>
2023-09-04 23:37:52 +0700

    made the login & register interface

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,46 @@
 'use client'
 import React from "react"
-import { Button } from 'antd'
+import { Button, Form, Radio } from 'antd'
+import Link from "next/link"
 
 export default function Login() {
+
+    const onFinish = (values: any) => {
+        console.log("success", values)
+    }
+
     return (
         <div className="flex justify-center h-screen items-center bg-primary">
-            <h1>Login</h1>
-            <Button
-                type="primary"
-                onClick={() => {
-                    alert("Hello");
-                }}
-            >
-                Primary Button
-            </Button>
+            <div className="card p-5 w-450">
+                <h1 className="text-xl">Job site- Login</h1>
+                <hr />
+                <Form layout='vertical' className="flex flex-col gap-5"
+                    onFinish={onFinish}
+                >
+                    <Form.Item label="Login As" name='userType'>
+                        <Radio.Group>
+                            <Radio value="employer">Employer</Radio>
+                            <Radio value="employee">Employee</Radio>
+                        </Radio.Group>
+                    </Form.Item>
+
+                    <Form.Item label="Email" name='email'>
+                        <input type="email" className="input" />
+                    </Form.Item>
+
+                    <Form.Item label="Password" name='password'>
+                        <input type="password" className="input" />
+                    </Form.Item>
+
+                    <Button type='primary' htmlType='submit' block>
+                        Login
+                    </Button>
+
+                    <Link href='/register'>
+                        Don't have an account? Register
+                    </Link>
+                </Form>
+            </div>
         </div>
     )
 }
