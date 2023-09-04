aecc366021965686d725a0647865d060a4e05162
Blackghost0315 <blackghost1503@gmail.com>
2023-09-04 10:30:08 +0700

    I am going to remove the tailwind & NextUI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,28 @@
 'use client'
 import React from "react"
-import { Button } from '@nextui-org/button'
+import { Card, CardHeader, CardBody, CardFooter, Image, Divider, Form } from '@nextui-org/react'
 
 export default function Login() {
     return (
         <div className="flex justify-center h-screen items-center bg-primary">
-            <Button 
-                color="primary" 
-                variant="shadow" 
-                onPress={() => {
-                    alert("Hello")
-                }}
-            >
-                Click me
-            </Button>
+            
+            <Card className = 'max-w-[400px]'>
+                <CardHeader className = 'flex flex-col'>
+                    <Image
+                    alt="nextui logo"
+                    height={100}
+                    radius="sm"
+                    src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+                    width={100}
+                    />
+                    <p className = 'text-md'>Blackghost's Job site - Login</p>    
+                </CardHeader>
+                <Divider/>
+                <CardBody>
+                    
+                </CardBody>
+            </Card>
+            
         </div>
     )
 }
