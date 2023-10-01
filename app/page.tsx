cba1240c68be440eb794d0ad82e936b2644672b2
Blackghost0315 <blackghost1503@gmail.com>
2023-10-01 11:00:08 +0700

    upgraded to Next 13.5.3

--- app/page.tsx
+++ app/page.tsx
@@ -37,32 +37,34 @@ export default function Home() {
   return (
     <div>
       <Filters filters={filters} setFilters={setFilters} getData={fetchJobs}/>
-      <Row gutter={[16, 16]} className='gap-3'>
+      <Row>
         {jobs.map((job:any) => (
           <Col 
             span={8} 
-            className='card flex flex-col gap-2 py-3 cursor-pointer'
+            className='p-2'
             key={job._id}
             onClick={() => router.push(`/jobinfo/${job._id}`)}
           >
-            <h1 className='text-md'>{job.title}</h1>
-            <Divider/>
-            <div className="flex justify-between">
-              <span>Company</span>
-              <span>{job.user.name}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Location</span>
-              <span>{job.location}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Salary</span>
-              <span>{job.salaryFromRange} USD - {job.salaryToRange} USD</span>
-            </div>
+            <div className='card flex flex-col gap-2 py-3 p-2 cursor-pointer'>
+              <h1 className='text-md'>{job.title}</h1>
+              <Divider/>
+              <div className="flex justify-between">
+                <span>Company</span>
+                <span>{job.user.name}</span>
+              </div>
+              <div className="flex justify-between">
+                <span>Location</span>
+                <span>{job.location}</span>
+              </div>
+              <div className="flex justify-between">
+                <span>Salary</span>
+                <span>{job.salaryFromRange} USD - {job.salaryToRange} USD</span>
+              </div>
 
-            <div className="flex justify-between">
-              <span>Work Mode</span>
-              <span>{job.workMode}</span>
+              <div className="flex justify-between">
+                <span>Work Mode</span>
+                <span>{job.workMode}</span>
+              </div>
             </div>
           </Col>
         ))}
